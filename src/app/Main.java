a086a44612a532886112e0a1031a4560533d194c
AleehSophia <alessia.casado@gmail.com>
2022-09-12 20:20:22 -0300

    :sparkles:feat: Position class from boardgame

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,10 @@
 package app;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
