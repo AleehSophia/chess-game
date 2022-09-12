bde52726258b4f964fb1bdfb5e0063f6fbffab33
AleehSophia <alessia.casado@gmail.com>
2022-09-12 20:37:27 -0300

    :sparkles:feat: Board and Piece implemented for the first time

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,11 @@
 package app;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
-        System.out.println(position);
+
+        Board board = new Board(8, 8);
     }
 }
