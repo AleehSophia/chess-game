a4044864d98914988bf3e4e1191509e57c2d44b6
AleehSophia <alessia.casado@gmail.com>
2022-09-12 21:02:54 -0300

    :sparkles:feat: UI created and implemented in main to print the board

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,10 +2,12 @@ package app;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
 
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
