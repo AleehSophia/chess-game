9d35ce2d18e72fec1247b4c4152259990feb2e44
AleehSophia <alessia.casado@gmail.com>
2022-09-13 09:50:34 -0300

    :ok_hand:fix: Adding defensive programming to Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -29,14 +29,23 @@ public class Board {
 
 
     public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
@@ -50,6 +59,9 @@ public class Board {
     }
 
     public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return piece(position) != null;
     }
 }
