dd0880a002ffee566126bceacd907c463f3024c2
AleehSophia <alessia.casado@gmail.com>
2022-09-20 13:00:24 -0300

    :bug:fix: en passant move fixed

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -48,7 +48,7 @@ public class Pawn extends ChessPiece {
                 }
                 Position right = new Position(position.getRow(), position.getColumn() + 1);
                 if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
-                    mat[right.getRow() - 1][left.getColumn()] = true;
+                    mat[right.getRow() - 1][right.getColumn()] = true;
                 }
             }
         }
@@ -79,7 +79,7 @@ public class Pawn extends ChessPiece {
                 }
                 Position right = new Position(position.getRow(), position.getColumn() + 1);
                 if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
-                    mat[right.getRow() + 1][left.getColumn()] = true;
+                    mat[right.getRow() + 1][right.getColumn()] = true;
                 }
             }
         }
